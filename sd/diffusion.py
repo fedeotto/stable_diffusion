45b6020d52024e051988fff88159b5cf2328dd55
FedeOtto <federicoottomano94@gmail.com>
2024-06-15 09:23:12 +0100

    updates diffusion

--- sd/diffusion.py
+++ sd/diffusion.py
@@ -21,6 +21,109 @@ class TimeEmbedding(nn.Module):
 
         # (1, 1280)
         return x
+    
+class UNET_ResidualBlock(nn.Module):
+
+    def __init__(self, in_channels: int, out_channels: int, n_time: int =1280):
+        super().__init__()
+
+        self.groupnorm_feature = nn.GroupNorm(32, in_channels)
+        self.conv_feature = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.linear_time = nn.Linear(n_time, out_channels)
+
+        self.groupnorm_merged = nn.GroupNorm(32, out_channels)
+        self.conv_merged = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+
+        if in_channels == out_channels:
+            self.residual_layer = nn.Identity()
+        else:
+            self.residual_layer = nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0)
+    
+    def forward(self, feature, time):
+        # feature: (batch_size, in_channels, height, width) this is the latent
+        # time: (1, 1280)
+        #note: in the forward pass we will match the latent with its timestamp
+        reside = feature
+
+        feature = self.groupnorm_feature(feature)
+
+        feature = F.silu(feature)
+
+        feature = self.conv_feature(feature)
+
+        time = F.silu(time)
+
+        time = self.linear_time(time)
+
+        merged = feature + time.unsqueeze(-1).unsqueeze(-1)
+
+        merged = self.groupnorm_merged(merged)
+
+        merged = F.silu(merged)
+
+        merged = self.conv_merged(merged)
+
+        return merged + self.residual_layer(reside)
+    
+
+class UNET_AttentionBlock(nn.Module):
+
+    def __init__(self, n_head: int, n_embd: int, d_context=768):
+        super().__init__()
+
+        channels = n_head * n_embd
+        self.groupnorm = nn.GroupNorm(32, channels, eps=1e-6)
+        self.conv_input = nn.Conv2d(channels, channels, kernel_size=1, padding=0)
+
+        self.layernorm_1 = nn.LayerNorm(channels)
+        self.attention_1 = SelfAttention(n_head, n_embd, in_proj_bias=False)
+
+        self.layernorm_2 = nn.LayerNorm(channels)
+        self.attention_2 = CrossAttention(n_head, channels, d_context, in_proj_bias=False)
+
+        self.layernorm_3 = nn.LayerNorm(channels)
+        self.linear_geglu_1 = nn.Linear(channels, 4 * channels * 2)
+
+        self.linear_geglu_2 = nn.Linear(4 * channels, channels)
+
+        self.conv_output = nn.Conv2d(channels, channels, kernel_size=1, padding=0)
+
+    def forward(self, x, context):
+        # (batch_size, features, height, width)
+        # context: (batch_size, seq_len, dim)
+
+        residue_long = x
+
+        x = self.groupnorm(x)
+
+        x = self.conv_input(x)
+
+        n, c, h, w = x.shape
+
+        # (batch_size, features, height, width) -> (batch_size, features, height * width)
+        x = x.view(n, c, h * w)
+
+        # (batch_size, features, height * width) -> (batch_size, height * width, features)
+        x = x.transpose(-1,-2)
+
+        # Normalization + Self Attention with skip connection
+        residue_short = x
+        x = self.layernorm_1(x)
+        self.attention_1(x)
+        x+= residue_short
+        residue_short = x
+
+        # Normalization + Cross Attention with skip connection
+        x = self.layernorm_2(x)
+
+        # Cross Attention
+        self.attention_2(x,context)
+
+        x += residue_short
+
+        residue_short = x
+
+
 
 class UpSample(nn.Module):
 
@@ -31,7 +134,7 @@ class UpSample(nn.Module):
     
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # (batch_size, featrues, height, width) -> (batch_size, features, height * 2, width * 2)
-        x = F.interpolate(x, scale_factor=2, mode='neartest')
+        x = F.interpolate(x, scale_factor=2, mode='nearest')
         return self.conv(x)
 
 class SwitchSequential(nn.Sequential):
@@ -136,6 +239,7 @@ class Unet_OutputLayer(nn.Module):
 
         x = self.conv(x)
 
+        # (batch_size, 4, height/8, width/8)
         return x
 class Diffusion(nn.Module):
 
